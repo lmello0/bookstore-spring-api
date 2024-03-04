ad11fd336cbd53a5a72ab01dcef474a35db809ca
lmello <melloluc02@gmail.com>
2024-03-04 11:00:42 -0300

    :test_tube: test: Adding initial tests

--- src/test/java/com/lmello/bookstore/BookstoreApplicationTests.java
+++ src/test/java/com/lmello/bookstore/BookstoreApplicationTests.java
@@ -1,14 +1,21 @@
 package com.lmello.bookstore;
 
+import com.lmello.bookstore.controller.BookController;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class BookstoreApplicationTests {
 
+    @Autowired
+    private BookController controller;
+
     @Test
     void contextLoads() {
-        assert true;
+        assertThat(controller).isNotNull();
     }
 
 }
